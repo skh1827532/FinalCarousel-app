86865e706079eb9e9fcb3b0c7ba03ea1d0681869
skh1827532 <skh1827532@gmail.com>
2022-03-29 21:27:40 +0500

    Recently created

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens:{
+      
+     
+    },
+    extend: {
+      fontFamily: {
+        Montserrat: ['Montserrat', "sans-serif"]
+      },
+      screens: {
+        xs:"400px",
+        sm:"639px",
+        md:"766px",
+        lg:"1023px",
+        xl:"1270px"
+        
+
+    },
+  },
+  
+  plugins: [],
+}
+}
+
+// module.exports = {
+//   theme: {
+//     extend: {
+//       screens: {
+//         'lg': '992px',
+//         // => @media (min-width: 992px) { ... }
+//       },
+//     },
+//   },
+// }
